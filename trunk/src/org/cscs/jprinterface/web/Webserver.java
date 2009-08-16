3b595fd4221de08eb358d93e44bf24627e5a13b0
shucksmith <shucksmith@06021292-3d5e-11de-90e1-f9f1724fc9cc>
2009-08-16 12:41:02 +0000

    servletmodified webserver to host servlet

--- trunk/src/org/cscs/jprinterface/web/Webserver.java
+++ trunk/src/org/cscs/jprinterface/web/Webserver.java
@@ -3,13 +3,14 @@ package org.cscs.jprinterface.web;
 import java.util.logging.Logger;
 
 import org.cscs.jprinterface.queue.QueueManager;
-import org.cscs.jprinterface.service.ClientSocketServer;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Webserver {
 
@@ -28,14 +29,18 @@ public class Webserver {
 	    SelectChannelConnector connector = new SelectChannelConnector();
 	    connector.setPort(8080);
 	    server.addConnector(connector);
-	 
+
+	    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addServlet(new ServletHolder(new HelloServlet()), "/data");
+        	    
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
         resource_handler.setWelcomeFiles(new String[]{ "index.html" }); 
-        resource_handler.setResourceBase(".");
+        resource_handler.setResourceBase("htdocs");
  
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        handlers.setHandlers(new Handler[] { resource_handler, context, new DefaultHandler() });
         server.setHandler(handlers);
         try {
         	server.start();
