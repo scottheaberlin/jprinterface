18978c62f82369973fa3dc879c30b29cad54d9e2
shucksmith <shucksmith@06021292-3d5e-11de-90e1-f9f1724fc9cc>
2010-01-18 00:46:06 +0000

    debug + multi-device support

--- ecolux-client/src/org/cscs/interfaces/DebugInputStream.java
+++ ecolux-client/src/org/cscs/interfaces/DebugInputStream.java
@@ -1,21 +1,27 @@
 package org.cscs.interfaces;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class DebugInputStream extends InputStream {
 	final private static String pad = "00000000";
-	private final InputStream wrapped;
-	public DebugInputStream(InputStream inputStream) {
+	final private InputStream wrapped;
+	final private AtomicBoolean debug;
+	
+	public DebugInputStream(AtomicBoolean debug, InputStream inputStream) {
 		this.wrapped = inputStream;
+		this.debug = debug;
 	}
 
 	@Override
 	public int read() throws IOException {
 		int b = wrapped.read();
-		String bs = Integer.toBinaryString(b);
-		String hs = Integer.toHexString(b);
-		System.out.println(String.format("%d >> recv %s%s %s%s", System.currentTimeMillis(), pad.substring(0, 8-bs.length()), bs, pad.substring(0,2-hs.length()), hs));
+		if (debug.get()) {
+			String bs = Integer.toBinaryString(b);
+			String hs = Integer.toHexString(b);
+			System.out.println(String.format("%d >> recv %s%s %s%s", System.currentTimeMillis(), pad.substring(0, 8-bs.length()), bs, pad.substring(0,2-hs.length()), hs));
+		}
 		return b;
 	}
 
