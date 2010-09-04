8cfc6449dab1c3af55827b8a21c356dadacb798b
shucksmith <shucksmith@06021292-3d5e-11de-90e1-f9f1724fc9cc>
2010-09-04 15:54:16 +0000

    fix driver read timeout during conversion on win32, implement data doubling for E3 in data mode

--- ecolux-client/src/org/cscs/interfaces/TemperatureTest.java
+++ ecolux-client/src/org/cscs/interfaces/TemperatureTest.java
@@ -2,11 +2,14 @@ package org.cscs.interfaces;
 
 import gnu.io.CommPortIdentifier;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 public class TemperatureTest {
@@ -21,22 +24,24 @@ public class TemperatureTest {
         	for (CommPortIdentifier port:ports) {
         		System.out.println(port.getName());
         	}
-            
-        	driver = new CommDriverDS2480B(CommPortIdentifier.getPortIdentifier("/dev/ttyUSB0"));
+            // String port = "/dev/ttyUSB0";
+            String port = "COM17";
+        	driver = new CommDriverDS2480B(CommPortIdentifier.getPortIdentifier(port));
         	
         	double temp = 0;
         	DateFormat df = new SimpleDateFormat("yyyyMMdd-HHmmss");
         	String fileName = "/home/chris/temps" + df.format(new Date()) + ".csv";
         	System.out.println("Writing to " + fileName);
+        	new File(fileName).getParentFile().mkdirs();
 			PrintWriter pw = new PrintWriter(fileName);
         	DateFormat precision = new SimpleDateFormat("yyyyMMdd HHmmss");
         	
         	while (true) {
-	        	Collection<String> devices = driver.enumerateBusDevices();
-	        	System.out.println(String.format("Found devices: %s", devices));
-
+	        	List<String> devices = new ArrayList<String>(driver.enumerateBusDevices());
+	        	System.out.println(String.format("Found %d devices: %s", devices.size(), devices));
+	        	Collections.shuffle(devices);
+	        	driver.DS18B20requestConversion(null);
 	        	for (String device: devices) {
-	        		driver.DS18B20requestConversion(device);
 	        		temp = driver.DS18B20readTemperature(device);
 	        		System.out.println(String.format(" Temperature %8.4f deg-C", temp));
 	        		pw.printf("%s,%s,%d,%8.4f\n", device, precision.format(new Date()), System.currentTimeMillis(), temp);	        		
